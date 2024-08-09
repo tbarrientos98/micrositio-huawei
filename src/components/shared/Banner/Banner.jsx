623a3151e4185dd77af4ab329a822ed376b9eef5
leonardo_arrieta <leonardo_arrieta@solutionbox.com.ar>
2024-08-09 17:56:14 -0300

    fix legacy props next image + mobile en banner

--- src/components/shared/Banner/Banner.jsx
+++ src/components/shared/Banner/Banner.jsx
@@ -11,38 +11,34 @@ const Banner = ({
     rightSubtitle,
 }) => {
     return (
-        <div className="container mx-auto p-4">
-            <div className="relative w-full h-72 md:h-72 lg:h-72 overflow-hidden rounded-xl shadow-xl">
-                <Image
-                    src={imageUrl}
-                    alt="Banner Image"
-                    layout="fill"
-                    objectFit="cover"
-                    className="w-full h-full"
-                    priority
-                />
-                <div className="absolute inset-0 flex items-center justify-between p-6 bg-black bg-opacity-40 rounded-xl">
-                    <div className="flex flex-col items-start text-left w-1/2">
-                        <h1 className="text-white text-2xl md:text-3xl lg:text-4xl font-bold">{leftTitle}</h1>
-                        <h3 className="text-white text-md md:text-md lg:text-md mt-2 font-semibold">{leftSubtitle}</h3>
-                        <div className="flex space-x-4 mt-8">
-                            <button
-                                className="text-md text-white px-6 py-2 rounded-full transition duration-300 hover:bg-gray-700 ease-in-out bg-black cursor-pointer"
-                            >
-                                {leftButtonText}
-                            </button>
-                            <button
-                                className="border-2 border-gray-800 text-md text-white py-2 px-6 rounded-full transition duration-300 ease-in-out cursor-pointer"
-                            >
-                                {leftButtonText2}
-                            </button>
-                        </div>
+        <div className="container relative w-full h-72 md:h-72 lg:h-72 overflow-hidden rounded-xl shadow-xl">
+            <Image
+                src={imageUrl}
+                alt="Banner Partners"
+                fill
+                style={{ objectFit: 'cover' }}
+                className="rounded-xl"
+            />
+            <div className="absolute inset-0 flex flex-col md:flex-row items-center md:items-center justify-between p-4 md:p-6 bg-black bg-opacity-40 rounded-xl">
+                <div className="flex flex-col items-start text-left w-full md:w-1/2 mb-4 md:mb-0">
+                    <h1 className="text-black text-xl md:text-2xl lg:text-3xl font-bold">{leftTitle}</h1>
+                    <h3 className="text-white text-sm md:text-md lg:text-lg mt-2 font-semibold">{leftSubtitle}</h3>
+                    <div className="flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 mt-4 md:mt-6">
+                        <button
+                            className="text-sm md:text-md px-4 md:px-6 py-2 rounded-full transition duration-300 hover:bg-gray-700 ease-in-out bg-black text-white cursor-pointer"
+                        >
+                            {leftButtonText}
+                        </button>
+                        <button
+                            className="border-2 border-gray-800 text-sm md:text-md py-2 px-4 md:px-6 rounded-full transition duration-300 ease-in-out text-white cursor-pointer"
+                        >
+                            {leftButtonText2}
+                        </button>
                     </div>
-
-                    <div className="flex flex-col items-start text-left w-1/2">
-                        <h1 className="text-white text-2xl md:text-3xl lg:text-4xl font-bold">{rightTitle}</h1>
-                        <h3 className="text-white text-md md:text-md lg:text-md mt-2 font-semibold">{rightSubtitle}</h3>
-                    </div>
+                </div>
+                <div className="flex flex-col items-start text-left w-full md:w-1/2">
+                    <h1 className="text-black text-xl md:text-2xl lg:text-3xl font-bold">{rightTitle}</h1>
+                    <h3 className="text-white text-sm md:text-md lg:text-lg mt-2 font-semibold">{rightSubtitle}</h3>
                 </div>
             </div>
         </div>
